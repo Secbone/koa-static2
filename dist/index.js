68777bb5603c8845159cf3535129e2b7d33663f2
Secbone <secbone@gmail.com>
2016-07-17 17:44:40 +0800

    change babel runtime

--- dist/index.js
+++ dist/index.js
@@ -4,19 +4,25 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _regenerator = require("babel-runtime/regenerator");
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _koaSend = require("koa-send");
 
 var _koaSend2 = _interopRequireDefault(_koaSend);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
-
 function serve(path, root) {
     return function () {
-        var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
+        var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(ctx, next) {
             var pathArr;
-            return regeneratorRuntime.wrap(function _callee$(_context) {
+            return _regenerator2.default.wrap(function _callee$(_context) {
                 while (1) {
                     switch (_context.prev = _context.next) {
                         case 0:
@@ -56,7 +62,7 @@ function serve(path, root) {
         }));
 
         return function (_x, _x2) {
-            return ref.apply(this, arguments);
+            return _ref.apply(this, arguments);
         };
     }();
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const gulp = require("gulp");
 const babel = require("gulp-babel");
@@ -6,7 +6,7 @@ const babel = require("gulp-babel");
 gulp.task("default", () => {
     gulp.src("index.js")
         .pipe(babel({
-            presets: ["es2015", "stage-3"]
+            plugins: ["transform-runtime"]
         }))
         .pipe(gulp.dest("dist/"));
 });
