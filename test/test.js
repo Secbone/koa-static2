2388b46f00a74515d7542314c5b9ad30c2f14033
Secbone <secbone@gmail.com>
2016-07-17 18:00:09 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 "use strict"
 
-let assert = require("assert");
+const assert = require("assert");
+const serve = require("../dist/index.js").default;
+const Koa = require("koa");
+const request = require("supertest");
 
 describe("Koa-static2", () => {
-    it("should be true", () => {
-        assert.equal(true, true);
-    })
-})
+    const app = new Koa();
+    app.use(serve("static", "./"));
+
+    app.use(ctx => {
+        ctx.body = "it's work!";
+    });
+
+    it("should work", done => {
+        request(app.listen())
+            .get("/")
+            .expect(200, done)
+    });
+});
