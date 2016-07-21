4d0c819ff234287bd03c03e5314219e225b380b6
Secbone <secbone@gmail.com>
2016-07-21 16:17:27 +0800

    remove babel

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-import send from "koa-send";
+"use strict"
 
-function serve(path, root) {
-    return async function(ctx, next) {
-        try{
+const send = require("koa-send");
+
+module.exports = function serve(path, root) {
+    return function(ctx, next) {
+        if(ctx.method == "HEAD" || ctx.method == "GET") {
+
             let pathArr = ctx.path.slice(1).split("/");
+
+            // match path
             if(path == pathArr[0]) {
-                return await send(ctx, pathArr.slice(1).join("/"), {root: root});
+                return send(ctx, pathArr.slice(1).join("/"), {root: root}).then(() => {
+                    return next();
+                });
             }
-        } catch(e) {
-            ctx.status = 404;
         }
 
         return next();
     }
-}
-
-module.exports = serve;
+};
--- test/test.js
+++ test/test.js
@@ -1,21 +1,17 @@
 "use strict"
 
 import assert from "assert";
-import serve from "../dist/index.js";
+import serve from "../index.js";
 import Koa from "koa";
 import request from "supertest";
 
 describe("Koa-static2", () => {
     const app = new Koa();
-    app.use(serve("static", "./"));
-
-    app.use(ctx => {
-        ctx.body = "it's work!";
-    });
+    app.use(serve("static", __dirname));
 
     it("should work", done => {
         request(app.listen())
-            .get("/")
-            .expect(200, done)
+            .get("/static/some.txt")
+            .expect(200, "text content\n", done)
     });
 });
